2f03e9ecdb77e6ad8e4820c8256a9d04d76de74d
danharapiuk <daniel.harapiuk@gmail.com>
2022-07-09 12:02:11 +0200

    test2

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './scss/Header.scss';
+
+function Home() {
+  return (
+    <div className="Header">
+      <header className="App-header">
+        <p>test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import './scss/Home.scss';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <p>test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
